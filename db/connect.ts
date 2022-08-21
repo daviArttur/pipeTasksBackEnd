a0f8e68768f66c4a942219cc5477489723bed9a7
Davi Artur <daviartur2004@gmail.com>
2022-08-21 19:32:46 -0300

    (feat) criar função para conectar ao banco de dados

---
+++ db/connect.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import _db from '../config/db'
+
+async function connect() {
+  try {
+    await mongoose.connect(_db.DbUrl)
+    console.log("Conectado com sucesso")
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default connect
