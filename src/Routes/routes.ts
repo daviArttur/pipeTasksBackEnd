bd7c6bfe4454af55493cc93a5a6968a466f06a94
Davi Artur <daviartur2004@gmail.com>
2022-08-28 16:03:53 -0300

    (feat) configurar rotas de requisição

---
+++ src/Routes/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import cadastryController from "../Controller/cadastry/cadastryUserController";
+
+const router = Router();
+
+router.route("/cadastry")
+  .post(cadastryController);
+
+export default router;
