3c07c8ed65ddaea3f504e33c66aacd275d26ffac
Davi Artur <daviartur2004@gmail.com>
2022-08-28 16:22:48 -0300

    (refactor) importar arquivo com dados do usuário, remover objeto desnecessário

--- src/Controller/cadastry/validation.test.ts
+++ src/Controller/cadastry/validation.test.ts
@@ -1,22 +1,17 @@
+import UserObjectExample from "../../helper/UserObjectExample";
 import Validation from "./validation";
 
-const UserInputValues = {
-  name: "Davi",
-  surname: "Artur",
-  email: "test@exmaple.com",
-  password: "Ex@mple123asd"
-};
 
 describe("test methods validate function", () => {
 
   it("should return the correct data in this body", () => {
-    const User = new Validation(UserInputValues);
+    const User = new Validation(UserObjectExample);
 
-    expect(User.body).toEqual(UserInputValues);
+    expect(User.body).toEqual(UserObjectExample);
   });
   
   it("body must have the necessary keys", () => {
-    const excludeKeyObjectUser: Partial<typeof UserInputValues> = Object.assign({}, UserInputValues);
+    const excludeKeyObjectUser: Partial<typeof UserObjectExample> = Object.assign({}, UserObjectExample);
     delete excludeKeyObjectUser.email;
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -31,33 +26,33 @@ describe("test methods validate function", () => {
     const newUserObject = {
       name: "   Davi",
       surname: "Artur    ",
-      email: "   test@exmaple.com  ",
-      password: "  Ex@mple123asd "
+      email: "   example@gmail.com  ",
+      password: "  @#7daSdq4 "
     };
 
     const User = new Validation(newUserObject);
     User.removeSpacing();
 
-    expect(User.body).toEqual(UserInputValues);
+    expect(User.body).toEqual(UserObjectExample);
   });
 
   it("return error, case, value contain more than one word", () => {
-    const modifyNameUser = Object.assign({}, UserInputValues);
+    const modifyNameUser = Object.assign({}, UserObjectExample);
     modifyNameUser.name = "Davi atur";
     const User1 = new Validation(modifyNameUser).containMoreThanOneWord();
     expect(User1).toBe("Confira se todos os campos foram preenchidos corretamente.");
 
-    const modifySurnameUser = Object.assign({}, UserInputValues);
+    const modifySurnameUser = Object.assign({}, UserObjectExample);
     modifySurnameUser.surname = "Artur Silva";
     const User2 = new Validation(modifySurnameUser).containMoreThanOneWord();
     expect(User2).toBe("Confira se todos os campos foram preenchidos corretamente.");
 
-    const modifyEmailUser = Object.assign({}, UserInputValues);
+    const modifyEmailUser = Object.assign({}, UserObjectExample);
     modifyEmailUser.email = "es t@exmaple.co";
     const User3 = new Validation(modifyEmailUser).containMoreThanOneWord();
     expect(User3).toBe("Confira se todos os campos foram preenchidos corretamente.");
 
-    const modifyPasswordUser = Object.assign({}, UserInputValues);
+    const modifyPasswordUser = Object.assign({}, UserObjectExample);
     modifyPasswordUser.password = "dsjhfasd asdfasdf";
     const User4 = new Validation(modifyPasswordUser).containMoreThanOneWord();
     expect(User4).toBe("Confira se todos os campos foram preenchidos corretamente.");
