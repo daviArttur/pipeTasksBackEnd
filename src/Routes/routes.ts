376923677ce02b482c3b1d514fae3b4c1b224c88
Davi Artur <daviartur2004@gmail.com>
2022-09-03 16:41:48 -0300

    (remove) remover arquivo único de rotas, para dividi-las

--- src/Routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import cadastryController from "../Controller/cadastry/cadastryUserController";
-
-const router = Router();
-
-router.route("/cadastry")
-  .post(cadastryController);
-
-export default router;
