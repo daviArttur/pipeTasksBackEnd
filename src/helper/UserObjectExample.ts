d46cea242e8ddc06c08863cd9f117734c8591738
Davi Artur <daviartur2004@gmail.com>
2022-09-03 16:22:06 -0300

    (feat) adicionar login + hash no objeto do usuário

--- src/helper/UserObjectExample.ts
+++ src/helper/UserObjectExample.ts
@@ -2,7 +2,9 @@ const UserObjectExample = {
   name: "Davi",
   surname: "Artur",
   email: "example@gmail.com",
-  password: "@#7daSdq4"
+  password: "@#7daSdq4",
+  hash: "$2a$10$rskDsC87X5GZP/kCouKwkeRUk/cRY4y2J.zKRZw3p0nqPKBoXCvQm",
+  id: "630b95baeeca6dd5312d45ed",
 };
 
 export default UserObjectExample;
