df35935c64655cc9adc2644de2e61d6115f0a85f
Davi Artur <daviartur2004@gmail.com>
2022-09-03 17:13:39 -0300

    (feat) adicionar Controller para login do usuário

---
+++ src/Controller/login/loginController.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Request, Response } from "express";
+import createJwtToken from "../../helper/jwt/createJwtToken";
+import ComparePassword from "../../helper/login/comparePassword";
+
+
+async function loginController (req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  const { status, message, id } = await new ComparePassword(email, password).getUserDb();
+  
+  if (status === 200 && id) {
+    const token = createJwtToken(id);
+    return res.status(status).json({ token: token });
+  } else if (status === 404 && message) {
+    return res.status(status).json({ message: message });
+  } else {
+    return res.status(500).end();
+  }
+}
+
+export default loginController;
