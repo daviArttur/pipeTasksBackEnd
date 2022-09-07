11b018c487d5f998cac579ccd084fb09398f6d9e
Davi Artur <daviartur2004@gmail.com>
2022-09-07 14:17:24 -0300

    (change) melhorar mensagem de resposta ao cadastrar usuário

--- src/Models/user/cadastry/cadastryUserModel.test.ts
+++ src/Models/user/cadastry/cadastryUserModel.test.ts
@@ -1,14 +1,60 @@
+import dotenv from "dotenv";
+dotenv.config();
 import cadastryUser from "./cadastryUserModel";
 import bcrypt from "bcryptjs";
 import UserObjectExample from "../../../helper/UserObjectExample";
+import connect from "../../../helper/test/connect";
+import mongoose from "mongoose";
 
-it("test method hash password before cadastry user", () => {
-  const { password } = UserObjectExample;
-  const User = new cadastryUser({ ...UserObjectExample });
+const newUser = { 
+  name: "Marks",
+  surname: "Alves",
+  email: "marksalves@example.com",
+  password: "F73b2D"
+};
+
+const existingUser = {
+  name: "User",
+  surname: "Default",
+  email: "default@example.com",
+  password: "F73b2D"
+};
+
+describe("unit test for cadastryUserModel.test.ts", () => {
+
+  beforeEach( async () => {
+    await connect(process.env.DB_USER!, process.env.DB_PASS!);
+  });
+
+  afterEach( async () => {
+    await mongoose.disconnect();
+  });
+
+  it("it must add a hash to the password", () => {
+    const { password } = UserObjectExample;
+    const User = new cadastryUser({ ...UserObjectExample });
+    
+    const comparePasswordWith = bcrypt.compareSync(password, User.hashCompare);
+    expect(comparePasswordWith).toBeTruthy();
   
-  const comparePasswordWith = bcrypt.compareSync(password, User.hashCompare);
-  expect(comparePasswordWith).toBeTruthy();
+    const compareHashedPasswordWithHashedPasswordHash = bcrypt.compareSync(User.hashCompare, User.hashPassword());
+    expect(compareHashedPasswordWithHashedPasswordHash).toBeTruthy();
+  });
 
-  const compareHashedPasswordWithHashedPasswordHash = bcrypt.compareSync(User.hashCompare, User.hashPassword());
-  expect(compareHashedPasswordWithHashedPasswordHash).toBeTruthy();
+  it("should be possible to register user", async () => {
+    const response = new cadastryUser(newUser);
+    const UserSaveStatus = await response.cadastryUserSchema();
+    
+    expect(UserSaveStatus.status === 201).toBeTruthy();
+    
+    const deleteUser = await response.userSchema.deleteOne();
+    expect(deleteUser).toBeTruthy();
+  });
+
+  it("should return status error 406 if the user already exists", async () => {
+    const response = new cadastryUser(existingUser);
+    const UserSaveStatus = await response.cadastryUserSchema();
+    
+    expect(UserSaveStatus.status === 406).toBeTruthy();
+  });
 });
