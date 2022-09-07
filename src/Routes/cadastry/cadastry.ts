d4ab66cd700c87108bb06fbf1cecbe35f354bfb2
Davi Artur <daviartur2004@gmail.com>
2022-09-07 19:08:20 -0300

    (fix) corrigir validação do request.body

--- src/Routes/cadastry/cadastry.ts
+++ src/Routes/cadastry/cadastry.ts
@@ -1,17 +1,20 @@
 import { Router } from "express";
+
+// Valitdator
 import { body } from "express-validator";
+
+// Controller
 import cadastryUserController, { bodyValidation } from "../../Controller/cadastry/cadastryUserController";
 
 const router = Router();
 
-router.use(bodyValidation);
-
-router.route("/cadastry")
-  .post([
-    body("name").isString().isAlpha().notEmpty().isLength({ max: 20 }),
-    body("surname").isString().isAlpha().notEmpty().isLength({ max: 20 }),
-    body("email").isEmail().toLowerCase(),
-    body("password").isLength({ min: 8 })
-  ], cadastryUserController);
+router.post("/cadastry",
+  body("name").isString().isAlpha().notEmpty().isLength({ max: 20 }),
+  body("surname").isString().isAlpha().notEmpty().isLength({ max: 20 }),
+  body("email").isEmail().toLowerCase(),
+  body("password").isLength({ min: 8 }),
+  bodyValidation,
+  cadastryUserController
+);
 
 export default router;
