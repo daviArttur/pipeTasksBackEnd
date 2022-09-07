ce1d809ea02bcda90d8a329117166e6f8b8aee8f
Davi Artur <daviartur2004@gmail.com>
2022-09-07 14:23:56 -0300

    (feat, fix) fechar conexão com o DB ao terminar testes, adicionar novos testes

--- src/Models/user/login/loginModel.test.ts
+++ src/Models/user/login/loginModel.test.ts
@@ -1,35 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
+import connect from "../../../helper/test/connect";
 import UserObjectExample from "../../../helper/UserObjectExample";
 import LoginModel from "./loginModel";
-
 const { name, surname, email, password } = UserObjectExample;
 
 describe("asdasd", () => {
- 
-  beforeEach( async () => {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjw0jbt.mongodb.net/?retryWrites=true&w=majority`);
+  
+  afterEach( async () => {
+    await mongoose.disconnect();
   });
-
-  afterEach(() => {
-    mongoose.disconnect();
-  });
-
-  it("Schema deve retornar objeto com email e string do cliente", () => {
+  
+  it("Schema must return object with e-mail and client string", async () => {
+    await connect(process.env.DB_USER!, process.env.DB_PASS!);
     const Login = new LoginModel( email, password ).createQuery();
 
     expect(Login.email).toBe(email);
     expect(Login.password).toBe(password);
-  });
-
-  it("Schema deve retornar busca no banco com dados do usuário", async () => {
-    const Login = await new LoginModel( email, password ).findUser();
-
-    expect(Login?.toObject().name).toBe(name);
-    expect(Login?.toObject().surname).toBe(surname);
-    expect(Login?.toObject().email).toBe(email);
-    expect(Login?.toObject().password).toBeTruthy();
-    expect(Login?.toObject()._id).toBeTruthy();
   });
 });
