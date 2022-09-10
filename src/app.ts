1ea45a8dccc7c2d57ca4311459c2c18eab5ead57
Davi Artur <daviartur2004@gmail.com>
2022-09-10 12:14:09 -0300

    (fix) remover app.listen() no ambiente de testes

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,37 @@
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+
+// Router
 import routerCadastry from "./Routes/cadastry/cadastry";
 import routerAuth from "./Routes/auth/auth";
+
+// Connect DB
 import connectDevDB from "../db/connect";
-import express from "express";
 import connectTestDB from "./helper/test/connect";
-
-
-const app = express();
-
-console.log(process.env.NODE_ENV);
-// Connect database
-if (process.env.NODE_ENV !== "test") {
-  app.listen(3000);
-} else {
-  app.listen(3001);
-}
 
 if (process.env.NODE_ENV === "test" || process.env.ENVIROMENT === "production") {
   connectTestDB(process.env.DB_USER!, process.env.DB_PASS!);
 } else {
+  console.log("Dev DB");
   connectDevDB();
 }
 
-// View body in request
+const app = express();
+
+// Connect database
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(3000);
+}
+
 app.use(express.json());
 
 // Routes
 
-//cadastry
 app.use("/", routerCadastry);
 
-//auth
 app.use("/", routerAuth);
-
 
 
 export default app;
