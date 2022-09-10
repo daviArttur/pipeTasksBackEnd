fa32ed7cd4690a505d031b74aff38effb88db691
Davi Artur <daviartur2004@gmail.com>
2022-09-10 12:28:21 -0300

    (feat) adicionar tipagens

--- src/interface/auth/authInterface.ts
+++ src/interface/auth/authInterface.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { Result, ValidationError } from "express-validator";
 
 export type AuthResponseType = Response<{
   token?: string,
-  message?: string
+  message?: string,
+  errors?: Result<ValidationError>
 }>
 
 export interface AuthRequestType extends Request {
