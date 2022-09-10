6acc255d0ab9e7c465b055d26f0fee8f46748a1a
Davi Artur <daviartur2004@gmail.com>
2022-09-10 16:54:44 -0300

    (feat) definir automaticamente o user e pass para conexão

--- src/helper/test/connect.ts
+++ src/helper/test/connect.ts
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config();
 
-const connect = async (user: string, password: string) => {
-  return await mongoose.connect(`mongodb+srv://${user}:${password}@tests.nd7hvei.mongodb.net/?retryWrites=true&w=majority`);
+const connect = async (user?: string, password?: string) => {
+  const USER = process.env.DB_USER || user;
+  const PASSWORD = process.env.DB_PASS || password;
+  return await mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@tests.nd7hvei.mongodb.net/?retryWrites=true&w=majority`);
 };
 
 export default connect;
