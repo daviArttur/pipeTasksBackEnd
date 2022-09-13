cf28796d3ddd7c7505ae0dbfd329826dda4cf31b
Davi Artur <daviartur2004@gmail.com>
2022-09-13 18:44:23 -0300

    (change) transformar função em classe

--- src/helper/jsonwt/tokenValidate.ts
+++ src/helper/jsonwt/tokenValidate.ts
@@ -2,26 +2,6 @@ import dotenv from "dotenv";
 import jwt, { JwtPayload } from "jsonwebtoken";
 dotenv.config();
 
-// export const tokenValidate = (token: string): { status: number, errors?: string, id?: string } => {
-
-//   const formatedToken = 
-
-//   try {
-//     const result = jwt.verify(formatedToken!, process.env.JWT_SECRET!) as JwtPayload & { id: string };
-//     if (!result.id) throw new Error(JSON.stringify(result));
-//     return { status: 200, id: result.id };
-//   } catch (err) {
-    
-//   }
-// };
-
-// const handleError = (message: string) => {
-  
-
-
-// };
-
-
 interface IDecodeToken {
   status: number;
   id?: string;
@@ -31,17 +11,17 @@ interface IDecodeToken {
 export class DecodeToken {
   protected token: string;
 
-  constructor(token: string) {
-    this.token = token;
+  constructor(JwtToken: string) {
+    this.token = JwtToken;
   }
 
-  public static format(token: string) {
+  private format(token: string) {
     return token.split(" ")[1];
   }
 
   public decode(): IDecodeToken {
     try {
-      const formatedToken = DecodeToken.format(this.token);
+      const formatedToken = this.format(this.token);
       const result = jwt.verify(formatedToken!, process.env.JWT_SECRET!) as JwtPayload & { id: string };
       if (!result.id) throw new Error(JSON.stringify(result));
       return { status: 200, id: result.id };
@@ -50,7 +30,7 @@ export class DecodeToken {
     }
   }
 
-  protected handleError(message: string) {
+  private handleError(message: string) {
     switch (message) {
     case "jwt expired":
       return { status: 403, errors: message };
